3314efb0bb37ef51f77b934ead04d83e1b09aad9
boak123 <boluwatifeakorede17@gmail.com>
2023-11-21 05:15:00 -0800

    updated

--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String
+from os import environ
 
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """ user for the class """
+    if (storage_engine == 'db'):
+        __tablename__ = 'users'
+        email = column(string(128), nullable=False)
+        password = column(string(128), nullable=False)
+        first_name = column(string(128), nullable=True)
+        last_name = column(string(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
